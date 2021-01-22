21e248f98ab7b27497ba02d539ff6b26a4c3672c
Karla Tovar <luis.leon@sofoscorp.com>
2021-01-21 21:42:10 -0400

    changes

--- Documents/netflix-ux/src/server.js
+++ Documents/netflix-ux/src/server.js
@@ -6,4 +6,5 @@ app.use(express.static(__dirname+'/dist/netflix-frontend'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/netflix-frontend/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
